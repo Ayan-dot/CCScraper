b764dc2b432cfd2dac9efcc7f18c3beca85b9b93
Ayan-dot <62365810+Ayan-dot@users.noreply.github.com>
2021-05-01 17:42:09 -0400

    refactoring

--- script.py
+++ script.py
@@ -1,35 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
-
-class Product:
-    link = ""
-    price = 0
-    name = ""
-
-    def __init__(self, href, prc, nm):
-        self.link = href
-        self.name = nm
-        self.price = float(prc)
-    def return_price(self):
-        return self.price
-    def return_name(self):
-        return self.name
-    def return_link(self):
-        return self.link
-    def check_stock(self):
-        temp_url = self.link
-        temp_page = requests.get(temp_url)
-        temp_parser = BeautifulSoup(temp_page.content, 'html.parser')
-        stockbox = temp_parser.find('div', class_="pi-prod-availability")
-        stocks_list = stockbox.find_all('span')
-        for i in stocks_list:
-            if 'not available online' in (i.text.strip(" ")).lower() or "out of stock" in (i.text.strip(" ")).lower():
-                continue
-            else:
-                return True
-        return False
-       
+from classes.Product import Product
+     
 
 keyword = input("Enter search parameter:")
 specify = input("Enter label criteria: ")
